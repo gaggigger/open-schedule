4f1be76d7fb0544acaea7c757a2c6b6a24351b45
Ralitera Solofo <solofo.ralitera@gmail.com>
2017-11-27 22:03:09 +0300

    AngularJs for calendar

--- www/src/assets/module_calendar/js/app.js
+++ www/src/assets/module_calendar/js/app.js
@@ -1,87 +1,130 @@
-$(document).ready(function() {
+angular.module('calendarApp', ['ngSanitize', 'ngMaterial']).controller('calendarController', [
+'$scope', '$http', '$sce', '$mdDialog',
+function ($scope, $http, $sce, $mdDialog) {
 
-  $(window).hashchange( function(){
-    // Reload calendar
-    $('#calendar').fullCalendar( 'refetchEvents' )
-  });
+  $scope.resources = [];
+  $scope.fUrl = url('?');
 
+  $scope.init = function() {
+    $scope.trackChanges();
+    $scope.initCalendar();
+  };
 
-  var furl = url('?');
-  var Resources = [];
-  // Get resources
-  $.ajax({
-    url: furl.resources,
-    dataType: 'json',
-    beforeSend: function(request) {
-      request.setRequestHeader("Authorization", furl.token);
-    },
-    success: function(resources) {
-      Resources = resources;
-    }
-  });
+  /**
+   * Track src change
+   */
+  $scope.trackChanges = function() {
+    $(window).hashchange( function(){
+      // Reload calendar
+      $('#calendar').fullCalendar('refetchEvents');
+    });
+  };
 
-  $('#calendar').fullCalendar({
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay,listWeek'
-    },
-    //defaultDate: '2017-11-12',
-    navLinks: true, // can click day/week names to navigate views
-    defaultView : 'agendaWeek',
+  $scope.initCalendar = function() {
+    // Get resources
+    $http({
+      url: $scope.fUrl.resources,
+      method: 'get',
+      headers: {
+        'Authorization': $scope.fUrl.token
+      }
+    }).then(function(response) {
+      $scope.resources = response.data;
+    }, function(response) {
+      // TODO error
+    });
 
-    slotDuration : '00:15:00',
-    minTime : '07:00:00',
-    maxTime : '20:00:00',
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      //defaultDate: '2017-11-12',
+      navLinks: true, // can click day/week names to navigate views
+      defaultView: 'agendaWeek',
 
-    weekNumbers: true,
-    weekNumbersWithinDays: true,
-    weekNumberCalculation: 'ISO',
+      slotDuration: '00:15:00',
+      minTime: '07:00:00',
+      maxTime: '20:00:00',
 
-    editable: true,
-    eventLimit: true, // allow "more" link when too many events
-    timeFormat: 'H(:mm)',
-    slotLabelFormat : 'H(:mm)',
+      weekNumbers: true,
+      weekNumbersWithinDays: true,
+      weekNumberCalculation: 'ISO',
 
-    events: function(start, end, timezone, callback) {
-      $.ajax({
-        url: furl.path,
-        dataType: 'json',
-        beforeSend: function(request) {
-          request.setRequestHeader("Authorization", furl.token);
-        },
-        data: {
-          items: url('#').items,
-          start: start.unix(),
-          end: end.unix()
-        },
-        success: function(doc) {
-          callback(doc);
-        }
-      });
-    },
-    dayClick: function(date, jsEvent, view) {
-      console.log(date, jsEvent, view);
-      var str = '';
-      Resources.map(function(item) {
-        str += '<div>';
-        str += '  <span>' + item.name + '</span>';
-        str += '</div>';
-      });
-      vex.dialog.open({
-        message: 'TITLE',
-        input: [
-          str
-        ].join(''),
-        callback: function (data) {
-          if (!data) {
-            return console.log('Cancelled')
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      timeFormat: 'H(:mm)',
+      slotLabelFormat: 'H(:mm)',
+
+      // Load all events
+      events: function (start, end, timezone, callback) {
+        $http({
+          url: $scope.fUrl.path,
+          method: 'get',
+          headers: {
+            'Authorization': $scope.fUrl.token
+          },
+          data: {
+            items: url('#').items,
+            start: start.unix(),
+            end: end.unix()
           }
-          console.log(data);
-        }
-      })
+        }).then(function(response) {
+          callback(response.data);
+        }, function(response) {
+          // TODO error
+        });
+      },
+      dayClick: function (date, jsEvent, view) {
+        /*
+        console.log(date, jsEvent, view);
+        var str = '';
+        Resources.map(function (item) {
+          str += '<div>';
+          str += '  <span>' + item.name + '</span>';
+          str += '</div>';
+        });
+        vex.dialog.open({
+          message: 'TITLE',
+          input: [
+            str
+          ].join(''),
+          callback: function (data) {
+            if (!data) {
+              return console.log('Cancelled')
+            }
+            console.log(data);
+          }
+        })
+        */
+        $mdDialog.show({
+          controller: DialogController,
+          templateUrl: 'resources.tmpl.html',
+          parent: angular.element(document.body),
+          //targetEvent: ev,
+          clickOutsideToClose:true
+        }).then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+          }, function() {
+            $scope.status = 'You cancelled the dialog.';
+          });
+      }
+    });
+  }
 
-    }
-  });
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
 
-});
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+
+}]);
