f241337188fc1e62ca32b65f50c5f56a251237d2
Ralitera Solofo <solofo-ralitera@users.noreply.github.com>
2018-04-08 21:40:00 +0300

    Update database.sql
    Test

--- database/database.sql
+++ database/database.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `os_attachments`
 --
 
+
 DROP TABLE IF EXISTS `os_attachments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
